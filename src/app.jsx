a4a1a223c630753f247039d975603e497d94f537
Vagish Gupta <vagishgupta@Vagishs-MacBook-Air.local>
2018-01-09 02:53:24 +0530

    React as a app

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PureComponent }  from 'react';
+//console.log(PureComponent);
+
+// module.exports  = 'PureComponent';
+
+class App extends Component {
+    super(props) {
+
+    }
+    render() {
+        return (
+            <div>this is Commponent </div>
+        )
+    } 
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './app.jsx';
+// var app = require('./app.jsx');
+// console.log(app);
+import { Apptest , Apptest1 }  from './test.js'
+import Apptest2 from './test2.js'
 
+// import Apptest1   from './test.js'
+
+Apptest1();
+Apptest2();
 ReactDOM.render(
-  <div>dasdad</div>,
+    <div>
+        <App />
+        <div>dasdad</div>
+    </div>
+    ,
   document.getElementById('myapp')
 );
